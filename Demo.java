9f44eac9a0940194c99c5c4aad7f30e58e30eacd
SeelamManasa-hub <seelammanasa9@gmail.com>
2025-04-25 14:44:07 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -3,5 +3,12 @@ Demo java
     public static void main(string args[])
     {
         print("we are listening workshop")
+            print("hai hello")
+        if
+        {
+            print("yes")
+                }
+        else
+        print("no")
         }
     }
